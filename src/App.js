a485211920e45b3cd3d5b7d06030e06b57dc8a2a
daief48 <daief20103048@gmail.com>
2023-04-22 12:23:01 +0600

    add dark mode in navbar

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import TextFrom from "./components/TextFrom";
 import About from "./components/About";
+import { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode("dark")
+      document.body.style.backgroundColor = '#0c146b'
+    }else{
+      setMode("light")
+      document.body.style.backgroundColor = 'white'
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils2" aboutText="About TextUtils"/>
+      <Navbar title="TextUtils2" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode}/>
       <div className="container my-3">
-      <TextFrom heading="Enter the text to analyze"/>
+      <TextFrom heading="Enter the text to analyze below" mode={mode}/>
       {/* <About/> */}
       </div>
     </>
--- src/components/TextFrom.js
+++ src/components/TextFrom.js
@@ -1,50 +1,59 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 export default function TextFrom(props) {
-    const handleUpClick = () =>{
-        console.log("Uppercase was clicked"+ text)
-        let newText = text.toUpperCase();
-        setText(newText)
-    }
-    const handleLoClick = () =>{
-        console.log("Uppercase was clicked"+ text)
-        let newText = text.toLowerCase();
-        setText(newText)
-    }
-    const handleclearClick = () =>{
-        console.log("Uppercase was clicked"+ text)
-        let newText = " ";
-        setText(newText)
-    }
-    
-    const handlecopyClick = () =>{
-      console.log("Uppercase was clicked"+ text)
-      let newText = " ";
-      setText(newText)
+  const handleUpClick = () => {
+    console.log("Uppercase was clicked" + text)
+    let newText = text.toUpperCase();
+    setText(newText)
   }
-    const handleOnChange = (event) =>{
-        console.log("On change")
-        setText(event.target.value)
-    }
-    const [text, setText] = useState("Enter text here");
+  const handleLoClick = () => {
+    console.log("Uppercase was clicked" + text)
+    let newText = text.toLowerCase();
+    setText(newText)
+  }
+  const handleclearClick = () => {
+    console.log("Uppercase was clicked" + text)
+    let newText = " ";
+    setText(newText)
+  }
+
+  const handlecopyClick = () => {
+    console.log("I am Copy");
+    let text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  }
+
+  const handleremovspaceClick = () => {
+    console.log("Remove extra spaces.");
+    let newtext = text.trim();
+    setText(newtext);
+  }
+  const handleOnChange = (event) => {
+    console.log("On change")
+    setText(event.target.value)
+  }
+  const [text, setText] = useState("Enter text here");
   return (
     <>
-    <div className="container">
+      <div className={`container text-${props.mode === 'dark'?'white':'dark'}`} text- style={{backgroundColor: props.mode === 'dark'?'#0c146b':'white'}}>
         <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
-        <button className="btn btn-primary my-2 mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-        <button className="btn btn-primary my-2 mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
-        <button className="btn btn-primary my-2 mx-2" onClick={handleclearClick}>Clear Text</button>
-        <button className="btn btn-primary my-2 mx-2" onClick={handlecopyClick}>Copy Text</button>
+        <div className="mb-3">
+          <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'?'#0c146b':'white',
+        color: props.mode === 'dark'?'white':'black'}} id="myBox" rows="8"></textarea>
+          <button className="btn btn-primary my-2 mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+          <button className="btn btn-primary my-2 mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+          <button className="btn btn-primary my-2 mx-2" onClick={handleclearClick}>Clear Text</button>
+          <button className="btn btn-primary my-2 mx-2" onClick={handlecopyClick}>Copy Text</button>
+          <button className="btn btn-primary my-2 mx-2" onClick={handleremovspaceClick}>Remove Extra Space</button>
+        </div>
       </div>
-    </div>
 
-    <div className="container my-2">
+      <div className={`container text-${props.mode === 'dark'?'white':'dark'} my-2`} style={{backgroundColor: props.mode === 'dark'?'#0c146b':'white'}}>
         <h1>Your text summary</h1>
         <p>{text.split(" ").length} words and {text.length} characters</p>
         <h2>Preview</h2>
-        <p>{text}</p>
-    </div>
+        <p>{text.length>0?text:"Enter something to preview it here"}</p>
+      </div>
     </>
   )
 }
